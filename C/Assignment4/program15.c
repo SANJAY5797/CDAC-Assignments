49e10d01fb4782286fb38f8f6990bd7cb8dcf030
sanjay kumar <sanjayanumandla666@gmail.com>
2019-03-07 12:15:48 +0530

    updated 15th program

--- C/Assignment4/program15.c
+++ C/Assignment4/program15.c
@@ -5,12 +5,14 @@ char rev[200];
 int strcomp(char s[100],char t[100])
 {
 	int i=0;
+	//running the loop till the characters do not match or reach the end of loop
 	while(s[i]==t[i])
 	{
 		if(s[i]=='\0')
 			return 0;
 		i++;
 	}
+	//returning difference
 	return s[i]-t[i];
 }
 char *strev(char s[100])
@@ -18,24 +20,30 @@ char *strev(char s[100])
 	int i=0,j=strlen(s)-1;
 	while(i<strlen(s))
 	{
+		//storing the each character in reverse order
 		rev[i]=s[j];
 		i++;j--;
 	}
+	//returning the address
 	return rev;
 }
 char *strconcat(char *src,char *dest)
 {
 	int i,j;
+	//running till the end of the destination string
 	for (i = 0; dest[i] != '\0'; i++)
         ;
+	//storing the character next to the last character of the original destination string
     for (j = 0; src[j] != '\0'; j++)
         dest[i+j] = src[j];
+	//putting a null character to mark the end
     dest[i+j] = '\0';
     return dest;
 }
 void strcopy(char *a,char *b)
 {
 	int i=0;
+	//copying character by character till the null character is encountered
 	while((a[i]=b[i])!='\0')
 	i++;
 	return ;
